f55a94aa9b09a98898678c5130ea00cd6523a005
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2024-09-11 20:05:17 +0300

    Create basic modules.

--- Modules/Articles/Routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::get('/select/articles', 'SelectController@articles')->name('articles.select');
-
-Route::apiResource('articles', 'Api\ArticleController')->only('index', 'show');
--- Modules/Pages/Routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::get('/select/pages', 'SelectController@pages')->name('pages.select');
-
-Route::apiResource('pages', 'Api\PageController')->only('index', 'show');
